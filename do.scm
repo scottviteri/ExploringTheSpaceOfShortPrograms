a14a5487858ce228b6327fc0548cfd648f33117c
Scott Viteri <scottviteri@gmail.com>
2018-02-03 21:52:39 -0500

    Cleaning up more unnecessary files

--- do.scm
+++
@@ -1,6 +0,0 @@
-(define (sum/display lst)
-  (do ((remaining lst (cdr remaining))
-       (final-sum 0 (+ final-sum (car remaining))))
-      ((null? remaining) final-sum)
-    (display (car remaining))
-    (newline)))
--- zot.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-num = int(sys.argv[1])
-print(bin(num)[2:], end='')
