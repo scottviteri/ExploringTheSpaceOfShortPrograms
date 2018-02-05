70dbb16e59a4c2a57726d6947540f7399e94c5e3
Scott Viteri <scottviteri@gmail.com>
2018-02-05 12:51:30 -0500

    Cached more program results

--- search.sh
+++ search.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-for ((i=500001; i<=1000000; i++)); do
+for ((i=1000001; i<=3000000; i++)); do
     OUTPUT="$(echo "obase=2;${i}" | bc | guile -s zot.scm)";
     echo "${OUTPUT}" >> "export2.txt";
 done
