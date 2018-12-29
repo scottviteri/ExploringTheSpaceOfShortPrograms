ad2b7353b9a01c0c7cc021466efd2de07f0a1b75
Scott Viteri <scottviteri@gmail.com>
2018-12-29 14:54:24 -0500

    Added pdf of ipynb file

---
+++ shell.nix
@@ -0,0 +1,19 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "env";
+  env = buildEnv { name = name; paths = buildInputs; };
+  buildInputs = [
+    python36
+    python36Packages.numpy
+    python36Packages.matplotlib
+    python36Packages.ipython
+    python36Packages.more-itertools
+    python36Packages.ipywidgets
+    python36Packages.scikitlearn 
+    python36Packages.seaborn
+    python36Packages.pydotplus
+    python36Packages.pandas
+
+  ];
+}
+
